3fa9e6923e0d82183273ea196683a87f493af144
Cosmin Constantin Lazar <cosminconstantinlazar@gmail.com>
2024-05-05 21:07:50 +0200

    Also load configuration from env

--- src/settings.rs
+++ src/settings.rs
@@ -1,5 +1,5 @@
 use crate::mitaffald::settings::AffaldVarmeConfig;
-use config::{Config, ConfigError, File};
+use config::{Config, ConfigError, Environment, File};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -13,7 +13,8 @@ impl Settings {
     pub fn new() -> Result<Self, ConfigError> {
         let settings = Config::builder()
             .add_source(File::with_name("config/default.toml"))
-            .add_source(File::with_name("config/secrets.toml"))
+            .add_source(File::with_name("config/secrets.toml").required(false))
+            .add_source(Environment::default())
             .build()?;
 
         settings.try_deserialize()
