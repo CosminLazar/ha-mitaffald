190cc5bd4e265469ff328d9819c82ab52f3c2768
Cosmin Constantin Lazar <cosminconstantinlazar@gmail.com>
2024-05-17 22:37:19 +0200

    Remove loop run and add

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,16 @@ use ha_mitaffald::sync_data;
 
 #[tokio::main]
 async fn main() {
-    loop {
-        println!("Starting data synchronization");
+    println!("Starting data synchronization");
 
-        let settings = Settings::new().expect("Failed to read settings");
-        let report_interval = tokio::time::Duration::from_secs(settings.reporting_interval_secs);
-        let report = sync_data(settings).await;
+    let settings = Settings::new().expect("Failed to read settings");
+    let report = sync_data(settings).await;
 
-        match report {
-            Ok(_) => println!("Data synchronization completed"),
-            Err(x) => eprintln!(
-                "Data synchronization failed (some entities may have been updated), error: {}",
-                x
-            ),
-        }
-
-        println!(
-            "Next synchronization will take place at: {}",
-            chrono::Local::now() + report_interval
-        );
-
-        tokio::time::sleep(report_interval).await;
+    match report {
+        Ok(_) => println!("Data synchronization completed"),
+        Err(x) => eprintln!(
+            "Data synchronization failed (some entities may have been updated), error: {}",
+            x
+        ),
     }
 }
