8bc8f2c09eb378b5c2028637a28952a3fb61efed
Cosmin Constantin Lazar <cosminconstantinlazar@gmail.com>
2024-05-23 21:06:09 +0200

    Update deploy script
    Introduce `_` as sepparator for env variables

--- scripts/deploy-latest-and-run.sh
+++ scripts/deploy-latest-and-run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 image_name="untagged6785/ha-mitaffald"
 container_name="ha-mitaffald"
 
@@ -12,4 +11,7 @@ echo "Removing container $container_name"
 docker rm $container_name || true
 
 echo "Starting new container"
-docker run -d --name $container_name -e affaldvarme.address.id -e mqtt.username -e mqtt.password --restart=on-failure:5 $image_name:latest
+docker run -d --name $container_name -e affaldvarme_address_id -e mqtt_username -e mqtt_password --restart=on-failure:5 $image_name:latest
+
+# run this via /etc/config/crontab and not via crontab -e, because QNAP...
+# crontab /etc/config/crontab && /etc/init.d/crond.sh restart
